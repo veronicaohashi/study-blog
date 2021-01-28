9b261f24628b551e7cac72e90bb962223c5d6545
veronicaohashi <veronica.ohashi@outlook.com>
2021-01-27 23:10:33 -0300

    initial commit

---
+++ src/test/kotlin/com/veronicaohashi/studyblog/StudyBlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.veronicaohashi.studyblog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudyBlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
