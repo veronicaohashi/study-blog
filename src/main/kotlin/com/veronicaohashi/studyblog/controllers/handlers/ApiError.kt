2cb22d537231ac64ea648ce7689129a83d9e8966
veronicaohashi <veronica.ohashi@outlook.com>
2021-02-14 22:30:49 -0300

    chore: validation error exception handler

--- src/main/kotlin/com/veronicaohashi/studyblog/controllers/handlers/ApiError.kt
+++ src/main/kotlin/com/veronicaohashi/studyblog/controllers/handlers/ApiError.kt
@@ -2,10 +2,13 @@ package com.veronicaohashi.studyblog.controllers.handlers
 
 import java.time.LocalDateTime
 
-data class ApiError (
+data class ApiError(
     val title: String,
     val status: Int,
     val details: String,
     val developerMessage: String,
-    val timestamp: LocalDateTime
-)
+    val timestamp: LocalDateTime,
+    val fieldDetails: List<Details>? = emptyList()
+) {
+  data class Details(val target: String, val message: String? = "")
+}
--- src/main/kotlin/com/veronicaohashi/studyblog/controllers/handlers/ExceptionHandler.kt
+++ src/main/kotlin/com/veronicaohashi/studyblog/controllers/handlers/ExceptionHandler.kt
@@ -4,10 +4,10 @@ import com.veronicaohashi.studyblog.domain.exception.BusinessException
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import java.time.LocalDateTime
-import kotlin.math.log
 
 @ControllerAdvice
 class ExceptionHandler {
@@ -31,4 +31,47 @@ class ExceptionHandler {
         )
   }
 
+  @ExceptionHandler(MethodArgumentNotValidException::class)
+  fun onValidationException(exception: MethodArgumentNotValidException): ResponseEntity<ApiError> {
+    logger.warn(exception.message, exception)
+
+    val details: MutableList<ApiError.Details> = mutableListOf()
+
+    exception.bindingResult.fieldErrors.forEach {
+      val fieldName = it.field
+      val fieldErrorMessage = it.defaultMessage
+
+      details.add(ApiError.Details(target = fieldName, message = fieldErrorMessage))
+    }
+
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(
+            ApiError(
+                timestamp = LocalDateTime.now(),
+                status = HttpStatus.BAD_REQUEST.value(),
+                title = "Business Request Exception, invalid fields",
+                details = exception.message ?: "",
+                developerMessage = exception.javaClass.name,
+                fieldDetails = details
+            )
+        )
+  }
+
+  @ExceptionHandler(Exception::class)
+  fun onInternalException(exception: Exception): ResponseEntity<ApiError> {
+    logger.warn(exception.message, exception)
+
+    return ResponseEntity
+        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+        .body(
+            ApiError(
+                timestamp = LocalDateTime.now(),
+                status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                title = "Internal Server Error",
+                details = exception.message ?: "",
+                developerMessage = exception.javaClass.name
+            )
+        )
+  }
 }
