9761a731652fada1eb49ae97acbfdab28820334c
veronicaohashi <veronica.ohashi@outlook.com>
2021-02-21 22:14:37 -0300

    fix: authors migration column

--- src/main/resources/db/migration/V20200221202900__create_authors_table.sql
+++ src/main/resources/db/migration/V20200221202900__create_authors_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE authors(
   id UUID PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  description TEXT,
+  about TEXT,
   created_at TIMESTAMP DEFAULT now(),
   updated_at TIMESTAMP DEFAULT now()
 );
